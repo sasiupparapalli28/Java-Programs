aea41d0f071f4a02f620a69f39ba12b0f7609a8d
UPPARAPALLI SURYA SASIKANTH <sasikanthupparapalli@gmail.com>
2025-03-13 20:41:13 +0530

    Create Swap1.java

---
+++ Basic/Swap1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Swap1{
+	public static void main(String[] args) {
+		Scanner ob= new Scanner(System.in);
+		int a = ob.nextInt();
+		int b = ob.nextInt();
+		System.out.println("Before swapping a & b : ");
+		System.out.println("a = "+a+", b = "+b);
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("After swapping a & b : ");
+		System.out.println("a = "+a+", b = "+b);
+	}
+}
+/*
+	OUTPUT
+  15
+  20
+	Before swapping a & b : 
+	a = 15, b = 20
+	After swapping a & b : 
+	a = 20, b = 15
+*/
