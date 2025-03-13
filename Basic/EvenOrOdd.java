9b3d1691f86759a46e1be4000d3e43b7fb3be293
UPPARAPALLI SURYA SASIKANTH <sasikanthupparapalli@gmail.com>
2025-03-13 20:05:57 +0530

    Update EvenOrOdd.java

--- Basic/EvenOrOdd.java
+++ Basic/EvenOrOdd.java
@@ -1,1 +1,20 @@
-
+import java.util.*;
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		Scanner ob = new Scanner(System.in);
+		int num = ob.nextInt();
+		if( num % 2 == 0)
+			System.out.println("EVEN");
+		else
+			System.out.println("ODD");
+	}
+}
+/*
+	OUTPUT
+  22
+	EVEN
+ 
+	OUTPUT
+  25
+	ODD
+*/
