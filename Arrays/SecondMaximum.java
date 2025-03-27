975b8f28c65a19372bf9c90d686f6d4b8e527a3c
UPPARAPALLI SURYA SASIKANTH <sasikanthupparapalli@gmail.com>
2025-03-27 20:26:09 +0530

    Create SecondMaximum.java

---
+++ Arrays/SecondMaximum.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+public class SecondMaximum {
+	public static void main(String[] args) {
+		int array[] = {10,32,34,54,19,29,38,45};
+		int max = array[0];
+		int secondMax = array[1];
+		if(max < secondMax){
+			max = array[1];
+			secondMax = array[0];
+		}
+		for(int i=2;i<array.length;i++){
+			if(array[i] > max){
+				secondMax = max;
+				max = array[i];
+			}else if(array[i] > secondMax){
+				secondMax = array[i];
+			}
+		}
+		
+		System.out.println("Given array is : "+Arrays.toString(array));
+		System.out.println("Max value in the array is : "+max);
+		System.out.println("Second max value in the array is : "+secondMax);
+	}
+}
+
+/*
+	OUTPUT
+	Given array is : 
+	[10, 32, 34, 54, 19, 29, 38, 45]
+	Max value in the array is : 54
+	Second max value in the array is : 45
+*/
